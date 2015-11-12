7e6891dac323ef33275ef375e0517906fe2faa25
Nicholas Olds <nolds9@yahoo.com>
2015-11-12 11:24:59 -0500

    hotfix: adds dependent destroy to user and song for favorites relationship

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
   validates :title, presence: true
 end

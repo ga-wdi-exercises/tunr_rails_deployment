22aabc85c82443742380b7946e8c276b6e897bdb
RobertAKARobin <robertgfthomas@gmail.com>
2015-11-12 09:15:58 -0500

    removed index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'artists#index'
   resources :artists do
-    resources :songs, only: [:index, :create, :new, :update]
+    resources :songs, only: [:create, :new, :update]
     resources :genres
   end
   resources :songs, only: [:index, :edit, :show, :destroy] do
